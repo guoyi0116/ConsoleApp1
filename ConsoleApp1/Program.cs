de98994b16b909c5848f91e7b52a3be5b17d70ab
user <user@A401-PC43>
2022-03-30 15:54:59 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static double ComputeBMI(int h, int w)
+
+        {
+            
+            return
+             w / Math.Pow((double)h / 100, 2);
+            
+                }
+        static void Main(string[] args)
+        {
+            //    Console.Write("n=");
+            //    int n=Convert.ToInt32(Console.ReadLine());
+            //    Random a = new Random();
+
+            //    int total=0;
+            //    for (int i=0;i<n; i++) 
+            //    {
+            //         int temp = a.Next(1, 100);
+            //        Console.WriteLine(temp);
+            //        total+= temp;
+            //    }
+            //    int result = total / n;
+            //    Console.Write("");
+            //    Console.WriteLine("平均"+result);
+            Console.WriteLine("height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("身高:{0},體重{1},BMI{2}",height,weight, ComputeBMI(height, weight));
+
+            Console.ReadLine();
+
+           
+           
+        }
+    }
+}
